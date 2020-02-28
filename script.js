7258ba41f241a23df05ec8aef928c3d5d47499f3
codywickman2 <49879926+codywickman2@users.noreply.github.com>
2020-02-28 14:47:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 function addImg() {
   var img = document.createElement("img");
   img.src = "waterpoloball.jpg";
-  img.onclick = function(e) { this.parentNode.removeChild(this)};
+  img.onclick = function(e) { this.parentNode.removeChild(this);};
   var class_name = "addImage";
   img.setAttribute("class", class_name);
   document.getElementById('newPic').appendChild(img);
+}
+
+
+function mouseMove(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var rgbColor = "rgb(" + x + "," + y + ")";
+    document.getElementById("div1").innerHTML = rgbColor;
+    
 }
