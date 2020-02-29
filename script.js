c3cc5747a88363db529b5f06799996cac60718f2
codywickman2 <49879926+codywickman2@users.noreply.github.com>
2020-02-29 00:47:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 function addImg() {
-  var img = document.createElement("img");
-  img.src = "waterpoloball.jpg";
-  img.onclick = function(e) { this.parentNode.removeChild(this);};
-  var class_name = "addImage";
-  img.setAttribute("class", class_name);
-  document.getElementById('newPic').appendChild(img);
+    var img = document.createElement("img");
+    img.src = "tonyA.jpg";
+    var class_name = "addImage";
+    var classN = "tonyA";
+    img.setAttribute("class", class_name);
+    var p = document.createElement("p");
+    var node = document.createTextNode("Tony Azevedo is a 5 time Olympian and is considered one of the greatest US Water Polo Players ever.");
+    p.appendChild(node);
+    var element = document.getElementById("newMessage");
+    element.appendChild(p);
+    p.setAttribute("class", classN);
+    document.getElementById('newPic').appendChild(img);
+    img.onclick = function(e) { 
+        this.parentNode.removeChild(this);
+        p.remove();
+    };
 }
 
 
 function mouseMove(e) {
     var x = e.clientX;
     var y = e.clientY;
-    var rgbColor = "rgb(" + x + "," + y + ")";
-    document.getElementById("div1").innerHTML = rgbColor;
-    
+    var coord = "Coordinates(" + x + "," + y + ")";
+    document.getElementById("div1").innerHTML = coord;
+
+}
+
+function mouseOut() {
+    document.getElementById("div1").innerHTML="";
+
 }
